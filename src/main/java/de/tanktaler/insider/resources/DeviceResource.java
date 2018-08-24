a6870ce89fc9adb888c8074de7de18985ed98851
Kanstantsin Kamkou <me@2ka.by>
2018-08-24 16:52:14 +0200

    Revert "mixed mode polishing"
    This reverts commit 89c69befbcb2b9743e102816872c2d7eb88b24ef.

--- src/main/java/de/tanktaler/insider/resources/DeviceResource.java
+++ src/main/java/de/tanktaler/insider/resources/DeviceResource.java
@@ -24,12 +24,9 @@ import de.tanktaler.insider.models.session.SessionConfidence;
 import de.tanktaler.insider.models.session.aggregation.DeviceConfidenceDto;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.jersey.caching.CacheControl;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
-import javax.inject.Named;
-import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -46,29 +43,27 @@ import org.mongodb.morphia.aggregation.Group;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public final class DeviceResource {
-  @Inject
-  @Named("dsInsider")
-  private Datastore dsInsider;
+  // @todo! move it to validators
+  private final Datastore dsInsider;
+  private final Datastore dsSession;
 
-  @Inject
-  @Named("dsSession")
-  private Datastore dsSession;
+  public DeviceResource(final Datastore dsInsider, final Datastore dsSession) {
+    this.dsInsider = dsInsider;
+    this.dsSession = dsSession;
+  }
 
   @Inject
   private Morphia morphia;
 
   @GET
   @Path("/{id}")
-  public Object fetchOne(
+  public Response fetchOne(
     @Auth final InsiderAuthPrincipal user,
     @PathParam("id") final ObjectId id
   ) {
-    final Device device = this.dsInsider.get(Device.class, id);
-    if (Objects.isNull(device)) {
-      return Response.status(Response.Status.NOT_FOUND).build();
-    }
-
-    return new ItemContentSingletonResource(device);
+    return Response.ok(new InsiderEnvelop(
+      this.morphia.toDBObject(this.dsInsider.get(Device.class, id))
+    )).build();
   }
 
   @GET
@@ -104,26 +99,4 @@ public final class DeviceResource {
       )
     ).build();
   }
-
-  @Singleton
-public class ItemContentSingletonResource {
-  private final Device device;
-
-  @Inject
-  private Morphia morphia;
-
-  public ItemContentSingletonResource(Device device) {
-    this.device = device;
-  }
-
-    @GET
-  @Path("/")
-  public Object fetchOne(
-    @Auth final InsiderAuthPrincipal user,
-    @PathParam("id") final ObjectId id
-  ) {
-
-    return Response.ok(new InsiderEnvelop(this.morphia.toDBObject(this.device))).build();
-  }
-}
 }
